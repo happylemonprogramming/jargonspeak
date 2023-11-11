dc4bd0cb50a47837c8fdbcd9ef7f65cea128a8cf
lemonknowsall <lemonknowsall@gmail.com>
2023-11-10 20:16:45 -0800

    created my own buildpack for heroku

--- ffmpeg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Download ffmpeg.exe from S3
-wget -O ffmpeg.exe https://privatevideotranslation.s3.us-west-1.amazonaws.com/ffmpeg.exe
-
-# Add the directory to PATH
-export PATH="/app:$PATH"
