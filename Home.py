d9f38cf8924854665735d34cb63823cb21d60f2f
lemonknowsall <lemonknowsall@gmail.com>
2023-11-22 23:26:10 -0800

    demo video to YouTube and image translate to default font

--- Home.py
+++ Home.py
@@ -14,7 +14,13 @@ st.info('''
         ''')
 
 st.text("Here's a demo:")
-st.video('demo.mp4')
+# st.video('demo.mp4')
+# YouTube video embed code
+youtube_embed_code = f'<iframe width="560" height="315" src="https://www.youtube.com/embed/NjDSLKn2A3E" frameborder="0" allowfullscreen></iframe>'
+
+# Display the YouTube video using st.markdown
+st.markdown(youtube_embed_code, unsafe_allow_html=True)
+
 
 if st.button('Donate :lightning:'):
     st.image('lightningaddress.png')
