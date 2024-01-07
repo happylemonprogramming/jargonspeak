1bb6b0978afb97715828732ce2ae869002af0746
lemonknowsall <lemonknowsall@gmail.com>
2024-01-06 22:01:04 -0800

    removed path from direct function

--- videofunctions.py
+++ videofunctions.py
@@ -142,7 +142,7 @@ def detectvideo(video, max_length, filepath, filename):
 	else:
 		try:
 			print('Video url detected')
-			duration = VideoFileClip(video, ffmpeg="/app/vendor/ffmpeg/ffmpeg").duration
+			duration = VideoFileClip(video).duration
 		except Exception as e:
 			return f"Error: {str(e)}"
 		if duration < max_length:
